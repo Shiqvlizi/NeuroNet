aafae7dfc335204cad64a318d28cc48b961324cc
Shiqvlizi Name <shiqvlizi@outlook.com>
2026-04-02 23:09:38 +0800

    feat: 好像能用了, 但是训练怎么这么慢

--- NeuroNet.cpp
+++ NeuroNet.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <random>
 #include <cmath>
+#include <string>
+#include <sstream>
 
 
 template<typename T>
@@ -14,7 +16,7 @@ using matrix = std::vector<std::vector<T>>;
 
 double learnRate = 0.01;
 
-std::vector<int> layerWidths = { 6,64,32,1 }; // 输入, ..., 输出
+std::vector<int> layerWidths = { 6,64,64,32,1 }; // 输入, ..., 输出
 int notOutputLayers = layerWidths.size() - 1;
 int outputHeight = layerWidths[layerWidths.size() - 1];
 
@@ -161,7 +163,7 @@ matrix<double> matrixMultiply_Num(double a, const matrix<double>& b)
 
 matrix<double> outerProduct(const std::vector<double>& a, const std::vector<double>& b)
 {
-	matrix<double> res(a.size(), std::vector<double>(b.size() - 1, 0));
+	matrix<double> res(a.size(), std::vector<double>(b.size(), 0));
 	for (int i = 0; i < a.size(); i++)
 	{
 		for (int j = 0; j < b.size(); j++)
@@ -307,7 +309,7 @@ std::vector<double> NeuroCalc(
 	}
 
 	rawInput[depth - 1] = matrixAdd(matrixMultiply(weightMatrixs[depth - 1], res), biasMatrixs[depth - 1]);
-	Input[depth - 1] = ReLU(rawInput[depth - 1]);
+	Input[depth - 1] = rawInput[depth - 1];
 	res = Input[depth - 1];
 
 	return res;
@@ -326,7 +328,7 @@ double loss(std::vector<double> NeuroNet_output, std::vector<double> train_outpu
 }
 
 
-void backPropagate(std::vector<double> trainInput, std::vector<double> neuroOutput, std::vector<double> rightOutput)
+void backPropagate(std::vector<double> trainInput, std::vector<double> rightOutput)
 {
 
 	NeuroCalc(trainInput, weightMatrixs, biasMatrixs, rawInput, Input);
@@ -334,35 +336,43 @@ void backPropagate(std::vector<double> trainInput, std::vector<double> neuroOutp
 
 	std::vector<double> delta = matrixMultiply_Num((2.0 / layerWidths.back()), matrixMinus(rawInput.back(), rightOutput));
 
-	int layerCnt = layerWidths.size() - 2;
+	int last = layerWidths.size() - 2;
+	const std::vector<double>& prevActLast = (last == 0) ? trainInput : Input[last - 1];
 
-	weightDiffMatrixs[layerCnt] = outerProduct(delta, Input[layerCnt]);
+
+	weightDiffMatrixs[last] = outerProduct(delta, prevActLast);
 
 
-	biasDiffMatrixs[layerCnt] = delta;
+	biasDiffMatrixs[last] = delta;
 
-	weightMatrixs[layerCnt] = matrixMinus(weightMatrixs[layerCnt], matrixMultiply_Num(learnRate, weightDiffMatrixs[layerCnt]));
 
-	biasMatrixs[layerCnt] = matrixMinus(biasMatrixs[layerCnt], matrixMultiply_Num(learnRate, biasDiffMatrixs[layerCnt]));
 
-	for (int i = layerCnt - 1; i >= 0; i--)
+	for (int i = last - 1; i >= 0; i--)
 	{
 
 		// 求 delta_i
 		delta = hadamardProduct(matrixMultiply(transpose(weightMatrixs[i + 1]), delta), dReLU(rawInput[i]));
 
+		const std::vector<double>& prevAct = (i == 0) ? trainInput : Input[i - 1];
 
-		weightDiffMatrixs[i] = outerProduct(delta, Input[i]);
+
+		weightDiffMatrixs[i] = outerProduct(delta, prevAct);
 
 
 		biasDiffMatrixs[i] = delta;
 
+
+
+
+
+	}
+
+	for (int i = last; i >= 0; i--)
+	{
+
 		weightMatrixs[i] = matrixMinus(weightMatrixs[i], matrixMultiply_Num(learnRate, weightDiffMatrixs[i]));
 
 		biasMatrixs[i] = matrixMinus(biasMatrixs[i], matrixMultiply_Num(learnRate, biasDiffMatrixs[i]));
-
-
-
 	}
 
 
@@ -400,12 +410,155 @@ void backPropagate(std::vector<double> trainInput, std::vector<double> neuroOutp
 
 
 
+// 自定义函数 自定义函数 自定义函数 自定义函数 自定义函数 自定义函数 自定义函数 自定义函数
 
 
 
 
 
+// 这是一个把输入形如 "2*3" 的字符串转化为 [2, 3, 0, 0, 1, 0] 的函数
+std::vector<double> stringToVector(std::string input)
+{
+	std::vector<double> res(6, 0);
+	std::stringstream ss(input);
 
+	double num1, num2;
+	char op;
+	if (ss >> num1 >> op >> num2)
+	{
+
+		/*if (num1 > 10 || num1 < -10 || num2 > 10 || num2 < -10)
+		{
+			std::print("输入范围: [-10, 10]");
+		}*/
+		res[0] = num1;
+		res[1] = num2;
+		if (op == '+')
+		{
+			res[2] = 1; // 加法
+		}
+		else if (op == '-')
+		{
+			res[3] = 1; // 减法
+		}
+		else if (op == '*')
+		{
+			res[4] = 1; // 乘法
+		}
+		else if (op == '/')
+		{
+			res[5] = 1; // 除法
+		}
+		else
+		{
+			std::cout << "不支持的运算符" << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "输入格式不正确" << std::endl;
+	}
+
+	return res;
+}
+
+double logNormalize(double x)
+{
+	double sign = (x >= 0.0 ? 1.0 : -1.0);
+
+	return sign * (log(abs(x) + 1.0)) / log(100.0);
+
+}
+
+
+std::vector<double> logNormalize(const std::vector<double>& x)
+{
+	int height = x.size();
+	std::vector<double> res(height);
+	for (int i = 0; i < x.size(); i++)
+	{
+		double sign = (x[i] >= 0.0 ? 1.0 : -1.0);
+
+		res[i] = sign * (log(abs(x[i]) + 1.0)) / log(100.0);
+	}
+	return res;
+
+}
+
+double inverseLogNormalize(double x)
+{
+	double sign = (x >= 0.0 ? 1.0 : -1.0);
+
+	return  sign * (pow(100.0, abs(x)) - 1.0);
+}
+
+
+std::vector<double> inverseLogNormalize(const std::vector<double>& x)
+{
+
+	int height = x.size();
+	std::vector<double> res(height);
+	for (int i = 0; i < height; i++)
+	{
+		double sign = (x[i] >= 0.0 ? 1.0 : -1.0);
+
+		res[i] = sign * (pow(100.0, abs(x[i])) - 1.0);
+	}
+	return res;
+}
+
+// 这是一个把 [2, 3, 0, 0, 1, 0] 变成 6 的函数
+double clacVector(std::vector<double> input)
+{
+	if (input[2] == 1)
+	{
+		return input[0] + input[1];
+	}
+	else if (input[3] == 1)
+	{
+		return input[0] - input[1];
+	}
+	else if (input[4] == 1)
+	{
+		return input[0] * input[1];
+	}
+	if (input[5] == 1)
+	{
+		return input[0] / input[1];
+	}
+	return 0.0;
+}
+
+
+// 这是一个把 [2, 3, 0, 0, 1, 0] 的矩阵变成 [0.2, 0.3, 0, 0, 1, 0] 的函数
+std::vector<double> vectorNorm(std::vector<double> input)
+{
+	input[0] /= 100;
+	input[1] /= 100;
+	return input;
+}
+
+// 这是一个提取输入矩阵的操作符的函数
+char vectorToOp(std::vector<double> input)
+{
+	if (input[2] == 1)
+	{
+		return '+';
+	}
+	else if (input[3] == 1)
+	{
+		return '-';
+	}
+	else if (input[4] == 1)
+	{
+		return '*';
+	}
+	if (input[5] == 1)
+	{
+		return '/';
+	}
+	return '?';
+}
 
 
 
@@ -420,8 +573,8 @@ int main()
 		weightMatrixs[layer].resize(layerWidths[layer + 1]);
 		weightDiffMatrixs[layer].resize(layerWidths[layer + 1]);
 
-		rawInput[layer].resize(layerWidths[layer]);
-		Input[layer].resize(layerWidths[layer]);
+		rawInput[layer].resize(layerWidths[layer + 1]);
+		Input[layer].resize(layerWidths[layer + 1]);
 		for (int i = 0; i < layerWidths[layer + 1]; i++)
 		{
 			weightMatrixs[layer][i].resize(layerWidths[layer]);
@@ -445,61 +598,121 @@ int main()
 
 
 
+	// 自由编辑
 
-
-
+	int casePerEpoch = 100;
 
 
 	// 此处是训练集
-	matrix<double> trainMatrixs_input(100, std::vector<double>(6, 0));
-	matrix<double> trainMatrixs_output(100, std::vector<double>(1, 0));
+	matrix<double> trainMatrixs_input(casePerEpoch, std::vector<double>(6, 0));
+	matrix<double> trainMatrixs_output(casePerEpoch, std::vector<double>(1, 0));
 	// [ [num1], [num2], [+], [-], [*], [/] ]
 	// [ [ans] ]
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
 
-	std::uniform_real_distribution<double> dist(-10, 10);
+	std::uniform_real_distribution<double> dist(-100, 100);
+	std::uniform_int_distribution<int> opDist(0, 3);
+	std::print("开始训练\n");
 
-	for (int i = 0; i < 100; i++)
+	int epoch = 500;
+
+	for (int i = 0; i < epoch; i++)
 	{
-		double num1 = dist(gen);
-		double num2 = dist(gen);
-		double op = dist(gen);
-		double ans;
 
-		trainMatrixs_input[i][0] = num1;
-		trainMatrixs_input[i][1] = num2;
+		// 每轮训练集 casePerEpoch 个
+		for (int j = 0; j < casePerEpoch; j++)
+		{
+			double num1 = dist(gen);
+			double num2 = dist(gen);
+			int op = opDist(gen);
+			double ans;
 
-		if (op < -5)
-		{
-			trainMatrixs_input[i][2] = 1;
-			ans = num1 + num2;
+			trainMatrixs_input[j][0] = num1;
+			trainMatrixs_input[j][1] = num2;
+
+			// 在生成每个样本时，先清空操作符 one-hot
+			std::fill(trainMatrixs_input[j].begin() + 2, trainMatrixs_input[j].end(), 0.0);
+
+			if (op == 0)
+			{
+				trainMatrixs_input[j][2] = 1;
+				ans = num1 + num2;
+			}
+			else if (op == 1)
+			{
+				trainMatrixs_input[j][3] = 1;
+				ans = num1 - num2;
+			}
+			else if (op == 2)
+			{
+				trainMatrixs_input[j][4] = 1;
+				ans = num1 * num2;
+			}
+			else
+			{
+				while (num2 >= -1 && num2 <= 1)
+				{
+					num2 = dist(gen);
+				}
+				trainMatrixs_input[j][1] = num2;
+				trainMatrixs_input[j][5] = 1;
+				ans = num1 / num2;
+			}
+			trainMatrixs_output[j][0] = ans;
+
+
+
+
 		}
-		else if (op < 0)
+
+
+
+
+		for (int j = 0; j < casePerEpoch; j++)
 		{
-			trainMatrixs_input[i][3] = 1;
-			ans = num1 - num2;
+			// std::vector<double> neuroOutput = NeuroCalc(trainMatrixs_input[j], weightMatrixs, biasMatrixs);
+
+			backPropagate(vectorNorm(trainMatrixs_input[j]), logNormalize(trainMatrixs_output[j]));
 		}
-		else if (op < 5)
-		{
-			trainMatrixs_input[i][4] = 1;
-			ans = num1 * num2;
-		}
-		else
-		{
-			trainMatrixs_input[i][5] = 1;
-			ans = num1 / num2;
-		}
-		trainMatrixs_output[i][0] = ans;
+
+
+		std::print("\r进度: {} / {}", i + 1, epoch);
+	}
+
+
+
+	std::print("\n完成!\n");
+	while (1)
+	{
+		std::print("input: ");
+		std::string input;
+		std::getline(std::cin, input);  // 读取整行
+		std::vector<double> inputVec = stringToVector(input);
+
+
+		std::vector<double> neuroAnsAfter = inverseLogNormalize(NeuroCalc(vectorNorm(inputVec), weightMatrixs, biasMatrixs));
+
+		double neuroRes = neuroAnsAfter[0];
+		double rightAns = clacVector(inputVec);
+
+
+		double delta = abs((rightAns - neuroRes) / rightAns) * 100;
+
+		std::print("\033[1A\r{} {} {} = {} | {} {:.2f}%\n", inputVec[0], vectorToOp(inputVec), inputVec[1], neuroRes, rightAns, delta);
+
+
 
 	}
 
 
 
 
-
-
 }
+
+
+
+
 
 
